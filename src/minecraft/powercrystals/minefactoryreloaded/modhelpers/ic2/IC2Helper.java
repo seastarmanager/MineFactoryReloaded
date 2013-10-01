2f7c95e1ce1484164e88935a94688aa6bad90dd6
samrg472 <samrg472@gmail.com>
2013-09-30 21:29:01 -0400

    Improve the IC2 helper class by not using reflection

--- src/minecraft/powercrystals/minefactoryreloaded/modhelpers/ic2/IC2Helper.java
+++ src/minecraft/powercrystals/minefactoryreloaded/modhelpers/ic2/IC2Helper.java
@@ -6,11 +6,10 @@ import ic2.api.energy.event.EnergyTileLoadEvent;
 import ic2.api.energy.event.EnergyTileUnloadEvent;
 import ic2.api.energy.tile.IEnergyTile;
 import ic2.api.item.Items;
-import ic2.api.recipe.IRecipeInput;
+import ic2.api.recipe.RecipeInputItemStack;
 import ic2.api.recipe.Recipes;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.common.MinecraftForge;
 import powercrystals.minefactoryreloaded.MFRRegistry;
 import powercrystals.minefactoryreloaded.MineFactoryReloadedCore;
@@ -19,13 +18,12 @@ import powercrystals.minefactoryreloaded.farmables.fertilizables.FertilizerStand
 import powercrystals.minefactoryreloaded.farmables.harvestables.HarvestableTreeLeaves;
 import powercrystals.minefactoryreloaded.tile.base.TileEntityFactoryPowered;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
-
 /**
  * Internal helper class, never call methods here except through the IC2 class as a mod layer
  */
 class IC2Helper {
+
+    private static final String notUpdated = "IC2 API is not up to date!";
 
     static void init() {
         try {
@@ -37,12 +35,10 @@ class IC2Helper {
             ItemStack stickyResin = Items.getItem("resin");
             ItemStack plantBall = Items.getItem("plantBall");
 
-            if (rubberSapling != null) {
+            if (rubberSapling != null)
                 MFRRegistry.registerFertilizable(new FertilizableIC2RubberTree(rubberSapling.itemID));
-            }
-            if (rubberLeaves != null) {
+            if (rubberLeaves != null)
                 MFRRegistry.registerHarvestable(new HarvestableTreeLeaves(rubberLeaves.itemID));
-            }
             if (rubberWood != null) {
                 MFRRegistry.registerHarvestable(new HarvestableIC2RubberWood(rubberWood.itemID, HarvestType.Tree, stickyResin.itemID));
                 MFRRegistry.registerFruitLogBlockId(((ItemBlock) rubberWood.getItem()).getBlockID());
@@ -56,40 +52,33 @@ class IC2Helper {
 
             MFRRegistry.registerHarvestable(new HarvestableIC2Crop(crop.itemID));
 
-            GameRegistry.addShapedRecipe(plantBall, new Object[]
-                    {
-                            "LLL",
-                            "L L",
-                            "LLL",
-                            Character.valueOf('L'), new ItemStack(MineFactoryReloadedCore.rubberLeavesBlock)
-                    });
+            GameRegistry.addShapedRecipe(plantBall,
+                    "LLL",
+                    "L L",
+                    "LLL",
+                    'L'  , new ItemStack(MineFactoryReloadedCore.rubberLeavesBlock));
 
-            try {
-                Method m = Recipes.extractor.getClass().getMethod("addRecipe", IRecipeInput.class, NBTTagCompound.class, ItemStack[].class);
-                m.invoke(Recipes.extractor, new RecipeInputItemStack(new ItemStack(MineFactoryReloadedCore.rubberSaplingBlock)), null, new ItemStack[]{rubber});
-            } catch (Throwable t) {
-                t.printStackTrace();
-            }
+            Recipes.extractor.addRecipe(new RecipeInputItemStack(new ItemStack(MineFactoryReloadedCore.rubberSaplingBlock)), null, rubber);
         } catch (Throwable t) {
-            FMLLog.warning("IC2 API is not up to date!");
+            FMLLog.warning(notUpdated);
             t.printStackTrace();
         }
     }
 
     static void postTileLoadEvent(TileEntityFactoryPowered tefp) {
         try {
-            Constructor con = EnergyTileLoadEvent.class.getConstructor(IEnergyTile.class);
-            MinecraftForge.EVENT_BUS.post((EnergyTileLoadEvent) con.newInstance(tefp));
+            MinecraftForge.EVENT_BUS.post(new EnergyTileLoadEvent((IEnergyTile) tefp));
         } catch (Throwable t) {
+            FMLLog.warning(notUpdated);
             t.printStackTrace();
         }
     }
 
     static void postTileUnloadEvent(TileEntityFactoryPowered tefp) {
         try {
-            Constructor con = EnergyTileUnloadEvent.class.getConstructor(IEnergyTile.class);
-            MinecraftForge.EVENT_BUS.post((EnergyTileUnloadEvent) con.newInstance(tefp));
+            MinecraftForge.EVENT_BUS.post(new EnergyTileUnloadEvent((IEnergyTile) tefp));
         } catch (Throwable t) {
+            FMLLog.warning(notUpdated);
             t.printStackTrace();
         }
     }
